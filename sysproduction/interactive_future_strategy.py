92e595a91c33c52f2264b94f82cced566b531c70
xiachenjun <xiachenjun1992@cloud.com>
2022-05-26 23:14:55 +0800

    添加国内期货定时,缓存每日14点的预测值，暂时手动维护csv

--- sysproduction/interactive_future_strategy.py
+++ sysproduction/interactive_future_strategy.py
@@ -2,7 +2,7 @@ from systems.provided.example.simplesystem import simplesystem
 from sysdata.config.configdata import Config
 from sysdata.sim.db_futures_sim_data import dbFuturesSimData
 from syslogdiag.emailing import send_mail_msg
-# from arctic import Arctic
+from arctic import Arctic
 
 def run_future_strategy():
 
@@ -28,20 +28,20 @@ def run_future_strategy():
         {"code": "TA", "name": "PTA"},
         {"code": "MA", "name": "甲醇"},
         {"code": "SR", "name": "白糖"},
-        {"code": "FU", "name": "燃油"}
+        # {"code": "FU", "name": "燃油"}
     ]
     for i in instruments:
         ser = my_system.combForecast.get_combined_forecast(i['code'])
         i['forecast'] = ser[ser.size - 1]
     instruments_sorted = sorted(instruments, key=lambda i: abs(i['forecast']), reverse=True)
-    # store = Arctic('localhost')
-    # library = store['simple_foreast']
+    store = Arctic('localhost')
+    library = store['simple_foreast']
     for i in instruments_sorted:
-        # item = library.read(i['code'])
-        # data = item.data
-        # data1 = data.append(my_system.combForecast.get_combined_forecast(i['code']).tail(1))
-        # data1.drop_duplicates(inplace=True)
-        str = str + i['code'] + ' ' + i['name'] + "\n" + "data1.to_string()" + "\n" + position.query(
+        item = library.read(i['code'])
+        data = item.data
+        data1 = data.append(my_system.combForecast.get_combined_forecast(i['code']).tail(1))
+        data1.drop_duplicates(inplace=True)
+        str = str + i['code'] + ' ' + i['name'] + "\n" + data1.to_string() + "\n" + position.query(
             "symbol=='" + i['code'] + "' & state=='o'").to_string() + "\n\n"
         # library.write(i['code'], data1)
     send_mail_msg(str, "国内期货策略")
