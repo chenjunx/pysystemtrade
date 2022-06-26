4ac72df7fe920a1473b2495692075b2039ae14f5
xiachenjun <xiachenjun1992@cloud.com>
2022-06-26 17:52:20 +0800

    添加报告

--- sysproduction/reporting/report_configs.py
+++ sysproduction/reporting/report_configs.py
@@ -42,40 +42,40 @@ class reportConfig(object):
 status_report_config = reportConfig(
     title="Status report",
     function="sysproduction.reporting.status_reporting.system_status",
-    output="email"
+    output="emailfile"
 )
 
 roll_report_config = reportConfig(
     title="Roll report",
     function="sysproduction.reporting.roll_report.roll_info",
     instrument_code=ALL_ROLL_INSTRUMENTS,
-    output="email"
+    output="emailfile"
 )
 
 daily_pandl_report_config = reportConfig(
     title="P&L report",
     function="sysproduction.reporting.pandl_report.pandl_info",
     calendar_days_back=1,
-    output="email"
+    output="emailfile"
 )
 
 reconcile_report_config = reportConfig(
     title="Reconcile report",
     function="sysproduction.reporting.reconcile_report.reconcile_info",
-    output="email"
+    output="emailfile"
 )
 
 trade_report_config = reportConfig(
     title="Trade report",
     function="sysproduction.reporting.trades_report.trades_info",
     calendar_days_back=1,
-    output="email"
+    output="emailfile"
 )
 
 strategy_report_config = reportConfig(
     title="Strategy report",
     function="sysproduction.reporting.strategies_report.strategy_report",
-    output="email"
+    output="emailfile"
 )
 
 risk_report_config = reportConfig(
@@ -87,14 +87,14 @@ risk_report_config = reportConfig(
 liquidity_report_config = reportConfig(
     title="Liquidity report",
     function="sysproduction.reporting.liquidity_report.liquidity_report",
-    output="email"
+    output="emailfile"
 
 )
 
 costs_report_config = reportConfig(
     title="Costs report",
     function="sysproduction.reporting.costs_report.costs_report",
-    output="email",
+    output="emailfile",
     calendar_days_back = 250
 
 )
@@ -103,41 +103,41 @@ slippage_report_config = reportConfig(
     title="Slippage report",
     function="sysproduction.reporting.slippage_report.slippage_report",
     calendar_days_back=250,
-    output="email"
+    output="emailfile"
 
 )
 
 instrument_risk_report_config = reportConfig(title="Instrument risk report",
     function=
     "sysproduction.reporting.instrument_risk_report.instrument_risk_report",
-                                             output="email"
+                                             output="emailfile"
                                              )
 
 min_capital_report_config= reportConfig(title="Minimum capital report",
     function=
     "sysproduction.reporting.minimum_capital_report.minimum_capital_report",
-                                        output="email"
+                                        output="emailfile"
                                         )
 
 duplicate_market_report_config = reportConfig(title="Duplicate markets report",
     function=
     "sysproduction.reporting.duplicate_market_report.duplicate_market_report",
-                                              output="email"
+                                              output="emailfile"
                                               )
 
 remove_markets_report_config = reportConfig(title="Remove markets report",
     function = "sysproduction.reporting.remove_markets_report.remove_markets_report",
-                                            output="email"
+                                            output="emailfile"
                                             )
 
 market_monitor_report_config = reportConfig(title = "Market monitor report",
         function = "sysproduction.reporting.market_monitor_report.market_monitor_report",
-                                            output="email")
+                                            output="emailfile")
 
 ## The reports will be run in this order
 report_config_defaults = dict(
-    # slippage_report = slippage_report_config,
-    # costs_report=costs_report_config,
+    slippage_report = slippage_report_config,
+    costs_report=costs_report_config,
     roll_report=roll_report_config,
     daily_pandl_report=daily_pandl_report_config,
     # reconcile_report=reconcile_report_config,
@@ -153,7 +153,4 @@ report_config_defaults = dict(
     market_monitor_report = market_monitor_report_config
     # liquidity_report=liquidity_report_config,
     # instrument_risk_report = instrument_risk_report_config,
-    # min_capital = min_capital_report_config,
-    # duplicate_market =duplicate_market_report_config,
-    # remove_markets_report = remove_markets_report_config
 )
