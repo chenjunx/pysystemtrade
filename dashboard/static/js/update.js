bd39c206813ab8dae0b23c42d9cbc76412633b7b
xiachenjun <xiachenjun1992@cloud.com>
2022-07-23 10:47:32 +0800

    web页面添加近期资金查看

--- dashboard/static/js/update.js
+++ dashboard/static/js/update.js
@@ -119,8 +119,7 @@ function update_pandl() {
       });
 
       $.each(data["captials_pandl"], function(k, v) {
-        $("#recent_captail_table tbody").append(`<tr>
-          <td>${v["time"]}</td>
+        $("#recent_captail_table tbody").append(`<tr><td>${v["time"]}</td>
           <td>${v["Accumulated"]}</td><td>${v["Actual"].toFixed(2)}</td>
           <td>${v["Broker"]}</td><td>${v["Max"].toFixed(2)}</td></tr>`);
       });
