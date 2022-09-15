2d32e7a1ee8faa498b59e1a7f04f424e6f422fd5
xiachenjun <xiachenjun1992@cloud.com>
2022-09-15 22:39:55 +0800

    更新获取数据部分

--- sysbrokers/mills/mills_contract_position_data.py
+++ sysbrokers/mills/mills_contract_position_data.py
@@ -5,7 +5,8 @@ from sysobjects.production.positions import listOfContractPositions
 from syscore.objects import arg_not_supplied, missing_contract
 from sysbrokers.mills.mills_connection import connectionMills
 from syslogdiag.log_to_screen import logtoscreen
-
+from sysobjects.production.positions import contractPosition, listOfContractPositions
+from sysobjects.contracts import futuresContract
 import json
 class millsContractPositionData(brokerContractPositionData):
     def __init__(self, millsconnection: connectionMills, log=logtoscreen("millsContractPositionData")):
@@ -14,7 +15,17 @@ class millsContractPositionData(brokerContractPositionData):
 
     def get_all_current_positions_as_list_with_contract_objects(self,
                                                                 account_id=arg_not_supplied) -> listOfContractPositions:
-        return listOfContractPositions(json.loads(self._millsconnection.query_posistions()))
+        raw_datas = json.loads(self._millsconnection.query_posistions())
+        pos = []
+        for raw_data in raw_datas:
+            contract = futuresContract(raw_data['instrument_code'], raw_data['expiry'])
+            contract_position_object = contractPosition(raw_data['position'], contract)
+            pos.append(contract_position_object)
+        list_of_contract_positions = listOfContractPositions(pos)
+        list_of_contract_positions_no_duplicates = (
+            list_of_contract_positions.sum_for_contract()
+        )
+        return list_of_contract_positions_no_duplicates
 
     def get_position_as_df_for_contract_object(self, *args, **kwargs):
         return super().get_position_as_df_for_contract_object(*args, **kwargs)
