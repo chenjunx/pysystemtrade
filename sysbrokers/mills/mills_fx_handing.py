e5406e617d8729df2eb4830baf02bd04c222b0c5
xiachenjun <xiachenjun1992@cloud.com>
2022-10-01 15:07:09 +0800

    调试dashapp,增加资金查看

--- sysbrokers/mills/mills_fx_handing.py
+++ sysbrokers/mills/mills_fx_handing.py
@@ -4,13 +4,20 @@ from sysbrokers.broker_trade import brokerTrade
 from sysbrokers.mills.mills_connection import connectionMills
 from syslogdiag.log_to_screen import logtoscreen
 from syscore.objects import arg_not_supplied
-
+import json
 class millsFxHandlingData(brokerFxHandlingData):
     def __init__(self, connection_Mills: connectionMills, log=logtoscreen("ibFXHandlingData")):
         self._connection_Mills = connection_Mills
 
     def broker_fx_balances(self, account_id: str = arg_not_supplied) -> dict:
-        pass
+        totalValues = json.loads(self._connection_Mills.query_excess_liquidity_value_across())
+        result = dict(
+            [
+                (summary_item['currency'], summary_item['value'])
+                for summary_item in totalValues
+            ]
+        )
+        return result
 
     def broker_fx_market_order(self, trade: float, ccy1: str, account_id: str = arg_not_supplied,
                                ccy2: str = "USD") -> brokerTrade:
