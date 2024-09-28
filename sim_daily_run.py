006ccebd6150bf37b7d328b7bffef026d3981ba9
xiachenjun <xiachenjun1992@cloud.com>
2024-09-28 15:06:13 +0800

    每日回测运行

---
+++ sim_daily_run.py
@@ -0,0 +1,32 @@
+from systems.basesystem import System
+from systems.risk import Risk
+from systems.provided.dynamic_small_system_optimise.accounts_stage import (
+    accountForOptimisedStage,
+)
+from systems.provided.dynamic_small_system_optimise.optimised_positions_stage import (
+    optimisedPositions,
+)
+from systems.portfolio import Portfolios
+from systems.positionsizing import PositionSizing
+from systems.provided.rob_system.rawdata import myFuturesRawData
+from systems.forecast_combine import ForecastCombine
+from systems.forecasting import Rules
+from systems.forecast_scale_cap import ForecastScaleCap
+from sysdata.sim.db_futures_sim_data import dbFuturesSimData
+from sysdata.config.configdata import Config
+
+data = dbFuturesSimData()
+
+my_system = System(stage_list=[Risk(),
+            accountForOptimisedStage(),
+            optimisedPositions(),
+            Portfolios(),
+            PositionSizing(),
+            myFuturesRawData(),
+            ForecastCombine(),
+            ForecastScaleCap(),
+            Rules()],
+                data=data,
+                config=Config("/home/xiachenjun/pysystemtrade/systems/provided/mills/mills_future_estimate_single.yaml")
+               )
+my_system.accounts.optimised_portfolio().percent.daily.to_csv("/home/xiachenjun/sim.csv")
