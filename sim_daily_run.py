2d387f6d76a9ea3b4496d2a26e19f70dcf4bc0fb
xiachenjun <xiachenjun1992@icloud.com>
2025-01-09 17:03:28 +0800

    添加调试

--- sim_daily_run.py
+++ sim_daily_run.py
@@ -18,6 +18,9 @@ from systems.provided.attenuate_vol.vol_attenuation_forecast_scale_cap import (
     volAttenForecastScaleCap,
 )
 data = dbFuturesSimData()
+import pydevd_pycharm
+pydevd_pycharm.settrace('192.168.50.117', port=12345, stdoutToServer=True,
+                        stderrToServer=True)
 
 my_system = System(stage_list=[Risk(),
             accountForOptimisedStage(),
