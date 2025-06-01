2f0e85596930723d08ca5b27aa001484678a4a3d
xiachenjun <xiachenjun1992@cloud.com>
2025-06-01 19:14:29 +0800

    调试前端

--- dashboard/app.py
+++ dashboard/app.py
@@ -209,9 +209,10 @@ def reconcile():
         retval["my"].set_index(
             ["instrument_code", "contract_date"], inplace=True, drop=False
         )
-    except:
+    except e:
         # IB gateway connection failed
         retval["gateway_ok"] = False
+        raise e
     if "optimal" in retval["optimal"].columns:
         # Force the underlying class to do the optimal position calc for us
         retval["optimal"]["optimal"] = retval["optimal"]["optimal"].astype(str)
